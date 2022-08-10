5f1d4a41d6b254109312a8f0f606b909fcf940b6
crazypig <crazypig@example.com>
2022-08-10 21:13:52 +0800

    #

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8888",
+        target: "http://localhost:3000",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
